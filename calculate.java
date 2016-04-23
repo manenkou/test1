ded02ce3076d6e029ef198ef0eada2e494c04a27
manenkou <a.manenkov@gmail.com>
2016-04-23 14:43:08 +0300

    create calculator

--- calculate.java
+++ calculate.java
@@ -2,5 +2,20 @@ public class calculate {
 	
 	public static void main(String[] arg) {
 		System.out.println("calculate...");
+		int a = Integer.valueOf(arg[0]);
+		int b = Integer.valueOf(arg[1]);
+		int summ = a + b;
+		int umn = a * b;
+		int dif = a - b;
+		int del = a / b;
+		int step1 = a * a;
+		int step2 = b * b;
+
+		System.out.println("Summ  " + summ);
+		System.out.println("Umnojenie   " + umn);
+		System.out.println("Vichetanie   " + dif);
+		System.out.println("Delenie   " + del);
+		System.out.println("Stepen1   " + step1);
+		System.out.println("Stepen2   " + step2);
 	}
 }
